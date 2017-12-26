b908a408a8063dbe0adb3226ae58d8cc418cdbd4
ZhouJianPing <zhoujianping24@hotmail.com>
2017-12-26 23:21:19 +0800

    修复照片裁剪后偏移的问题和旋转后点跟线缩小的问题

---
+++ UIImage+JPExtension.h
@@ -0,0 +1,12 @@
+//
+//  UIImage+JPExtension.h
+//  JPImageresizerView
+//
+//  Created by 周健平 on 2017/12/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (JPExtension)
+
+@end
---
+++ UIImage+JPExtension.m
@@ -0,0 +1,12 @@
+//
+//  UIImage+JPExtension.m
+//  JPImageresizerView
+//
+//  Created by 周健平 on 2017/12/26.
+//
+
+#import "UIImage+JPExtension.h"
+
+@implementation UIImage (JPExtension)
+
+@end
