c2730a09444f4512be048a4dd8d00ea5fbf2a8c6
zhoujianping <zhoujianping24@hotmail.com>
2021-06-05 09:10:27 +0800

    remove pod

--- Example/Pods/Target Support Files/JPCrop/JPCrop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JPCrop : NSObject
-@end
-@implementation PodsDummy_JPCrop
-@end
--- Example/Pods/Target Support Files/JPLiquidLayout/JPLiquidLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JPLiquidLayout : NSObject
-@end
-@implementation PodsDummy_JPLiquidLayout
-@end
