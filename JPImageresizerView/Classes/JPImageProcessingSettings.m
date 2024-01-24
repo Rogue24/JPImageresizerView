5637431b51500ed5c0d0a3c95c3fa136a3af2b42
zhoujianping <zhoujianping24@hotmail.com>
2024-01-24 19:36:39 +0800

    修正判断逻辑

--- JPImageresizerView/Classes/JPImageProcessingSettings.m
+++ JPImageresizerView/Classes/JPImageProcessingSettings.m
@@ -10,27 +10,15 @@
 @implementation JPImageProcessingSettings
 
 - (BOOL)isNeedProcessing {
-    if (self.cornerRadius > 0) {
-        return YES;
-    }
-    
-    if (self.backgroundColor) {
-        return YES;
-    }
-    
-    if (self.borderWidth >= 1 && self.borderColor) {
-        return YES;
-    }
-    
-    if (self.outlineStrokeWidth >= 1 && self.outlineStrokeColor) {
-        return YES;
-    }
-    
-    if (self.padding.top >= 1 || self.padding.left >= 1 || self.padding.bottom >= 1 || self.padding.right >= 1) {
-        return YES;
-    }
-    
-    return self.isOnlyDrawOutline;
+    if (self.cornerRadius > 0) return YES;
+    if (self.backgroundColor) return YES;
+    if (self.borderColor && self.borderWidth >= 1) return YES;
+    if (self.outlineStrokeColor && (self.outlineStrokeWidth >= 1 || self.isOnlyDrawOutline)) return YES;
+    if (self.padding.top >= 1 || 
+        self.padding.left >= 1 ||
+        self.padding.bottom >= 1 ||
+        self.padding.right >= 1) return YES;
+    return NO;
 }
 
 @end
